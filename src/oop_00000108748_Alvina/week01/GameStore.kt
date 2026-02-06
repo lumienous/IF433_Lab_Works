1dafe437f48380f0b84322e526075cec6eef9f1f
Vin <alvinaaklv@gmail.com>
2026-02-06 11:30:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000108748_Alvina/week01/GameStore.kt
+++ src/oop_00000108748_Alvina/week01/GameStore.kt
@@ -3,4 +3,9 @@ package oop_00000108748_Alvina.week01
 fun main () {
     val gameTitle : String = "Hogwarts Legacy"
     val price : Int = 819000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int) =  if (price > 500000) price * 20/100 else price * 10/100
