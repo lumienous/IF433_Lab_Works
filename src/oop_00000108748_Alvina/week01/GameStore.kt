06c9ccb6c73857d975c82dae8c5d479102e72a92
Vin <alvinaaklv@gmail.com>
2026-02-06 11:31:41 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000108748_Alvina/week01/GameStore.kt
+++ src/oop_00000108748_Alvina/week01/GameStore.kt
@@ -6,6 +6,13 @@ fun main () {
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+    printReceipt(gameTitle, price, finalPrice)
 }
 
 fun calculateDiscount(price: Int) =  if (price > 500000) price * 20/100 else price * 10/100
+
+fun printReceipt (gameTitle: String, price: Int, finalPrice: Int) {
+    println("Judul: $gameTitle")
+    println("Harga Awal: $price")
+    println("Harga Akhir: $finalPrice")
+}
