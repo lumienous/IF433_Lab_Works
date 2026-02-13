a7f324af433b51b216861bbda3f81243ceb8dbab
Vin <alvinaaklv@gmail.com>
2026-02-13 10:33:56 +0700

    week02: add warning validation in init block

--- src/oop_00000108748_Alvina/week02/Student.kt
+++ src/oop_00000108748_Alvina/week02/Student.kt
@@ -1,9 +1,12 @@
 package oop_00000108748_Alvina.week02
 
-class Student(
-    var name: String,
-    var nim: String,
-    var major: String
-) {
-    // Body class
+class Student(var name: String, var nim: String, var major: String) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Students $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
