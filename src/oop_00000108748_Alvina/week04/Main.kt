346ce6f0af6f504a590edbbac291176aea13657a
Vin <alvinaaklv@gmail.com>
2026-02-27 10:02:26 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000108748_Alvina/week04/Main.kt
+++ src/oop_00000108748_Alvina/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val electricCar = ElectricCar("BYD", 4, 100)
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
 }
