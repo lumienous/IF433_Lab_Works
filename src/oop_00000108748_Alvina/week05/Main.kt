f2139194a39070564ca5c987ab1eab36c45bf8af
Vin <alvinaaklv@gmail.com>
2026-03-06 10:20:07 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000108748_Alvina/week05/Main.kt
@@ -0,0 +1,26 @@
+package oop_00000108748_Alvina.week05
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+
+            }
+        }
+
+        println("--------------------------")
+    }
+}
