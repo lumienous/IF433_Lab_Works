39f401ed99e93d3f61fc1064f4942dfd5ef4a6b4
Vin <alvinaaklv@gmail.com>
2026-03-06 10:32:09 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000108748_Alvina/week05/MainTask2.kt
@@ -0,0 +1,13 @@
+package oop_00000108748_Alvina.week05
+
+fun main() {
+    val gopay = EWallet("Nyoman Ayu Carmenita", 50000.0)
+    val hanaBank = CreditCard ("Ye-on", 100000.0)
+
+    val daftarPayment: List<PaymentMethod> = listOf(gopay, hanaBank)
+    println("=== PEMBAYARAN DUBAI CHEWY COOKIE ===")
+    for (PaymentMethod in daftarPayment) {
+        print("[${PaymentMethod.accountName}] ")
+        PaymentMethod.processPayment(75000.0)
+    }
+}
