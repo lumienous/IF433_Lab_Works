0d1e039566f9115644c8d2fab56db7e8923e8d28
Vin <alvinaaklv@gmail.com>
2026-03-13 11:19:46 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000108748_Alvina/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000108748_Alvina.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
