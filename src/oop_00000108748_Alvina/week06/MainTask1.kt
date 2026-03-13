53c3242ecaeb83f7b5094a7fa254b7ee4c5f1bc1
Vin <alvinaaklv@gmail.com>
2026-03-13 11:26:56 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000108748_Alvina/week06/MainTask1.kt
@@ -0,0 +1,7 @@
+package oop_00000108748_Alvina.week06
+
+fun main() {
+    val lampu = SmartLamp("001", "Ruang Tamu")
+    val speaker = SmartSpeaker("002", "Google Nest Dapur")
+    val cctv = SmartCCTV("003", "Ezviz Garasi")
+}
