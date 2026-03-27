a0eac9892570e498a876986dad691aca668b0eef
Vin <alvinaaklv@gmail.com>
2026-03-27 11:46:21 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_00000108748_Alvina/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_00000108748_Alvina.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
