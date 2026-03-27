b94d1d0640bb2b9c435b1892c76a1cc7513b90e8
Vin <alvinaaklv@gmail.com>
2026-03-27 12:10:06 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000108748_Alvina/week07/MainTugas.kt
+++ src/oop_00000108748_Alvina/week07/MainTugas.kt
@@ -3,4 +3,14 @@ package oop_00000108748_Alvina.week07
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\nDrop Chance Senjata Legendary: ${ItemRarity.LEGENDARY.dropChance}")
+    val senjata = Weapon.forgeStarterSword()
+    println("\n=== DETAIL SENJATA ===")
+    println("Nama           : ${senjata.item.name}")
+    println("Damage         : ${senjata.item.damage}")
+    println("Rarity         : ${senjata.item.rarity}")
+    println("Drop Chance    : ${senjata.item.rarity.dropChance}")
+    println("Durability     : ${senjata.durability}")
+
 }
