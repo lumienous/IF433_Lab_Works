ad483819d6c7d91bbffdadf64ed409de78edea9c
Vin <alvinaaklv@gmail.com>
2026-04-17 10:49:54 +0700

    week08: test safe casting in main

--- src/oop_00000108748_Alvina/week08/Main.kt
+++ src/oop_00000108748_Alvina/week08/Main.kt
@@ -35,7 +35,7 @@ fun main() {
         }
     }
 
-    val someObject: Any = 100 // Tipe aslinya Integer
+    val someObject: Any = 100
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast + fallback: $safeString")
 }
