ac87b6c5b1de41766e0c1f6646d6bb8f1e46302b
Vin <alvinaaklv@gmail.com>
2026-05-07 12:57:18 +0700

    week10: (task) populate coin repository

--- src/oop_00000108748_Alvina/week10/CryptoDashboard.kt
+++ src/oop_00000108748_Alvina/week10/CryptoDashboard.kt
@@ -2,4 +2,11 @@ package oop_00000108748_Alvina.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    val BTC = Coin("BTC", 2.0)
+    val ETH= Coin("ETH", 3.0)
+    val USDT = Coin("USDT", 1.5)
+
+    coinRepo.add(BTC)
+    coinRepo.add(ETH)
+    coinRepo.add(USDT)
 }
