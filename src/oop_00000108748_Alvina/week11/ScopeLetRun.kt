705b4d19ed2df9acf546d620de9fdbfcd6589562
Vin <alvinaaklv@gmail.com>
2026-05-08 10:41:18 +0700

    week11: implement let function for null safety

---
+++ src/oop_00000108748_Alvina/week11/ScopeLetRun.kt
@@ -0,0 +1,12 @@
+package oop_00000108748_Alvina.week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Length: $length")
+}
