d828e55a728cbc7eeecd5ca36e9646ce2c7ccb13
Vin <alvinaaklv@gmail.com>
2026-05-08 11:28:02 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000108748_Alvina/week11/SmartHomeDashboard.kt
+++ src/oop_00000108748_Alvina/week11/SmartHomeDashboard.kt
@@ -16,4 +16,14 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+
+    val AC_Inverter = run{
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(AC_Inverter)
+
+    val petCare = run{
+        SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10)
+    }
+    homeDevices.add(petCare)
 }
