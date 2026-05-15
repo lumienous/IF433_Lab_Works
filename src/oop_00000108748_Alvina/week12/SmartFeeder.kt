b5b2e566f73aa0b01119b60dfd21afb6b103ea20
Vin <alvinaaklv@gmail.com>
2026-05-15 11:11:03 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000108748_Alvina/week12/SmartFeeder.kt
+++ src/oop_00000108748_Alvina/week12/SmartFeeder.kt
@@ -32,5 +32,8 @@ fun main() {
 
     runCatching {
         dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    } .onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr.")
     }
 }
