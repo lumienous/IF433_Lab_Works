8715f2c2a5b12a33f5b08558ec0da7702fb58678
Vin <alvinaaklv@gmail.com>
2026-05-15 10:58:27 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000108748_Alvina/week12/SmartFeeder.kt
+++ src/oop_00000108748_Alvina/week12/SmartFeeder.kt
@@ -2,5 +2,9 @@ package oop_00000108748_Alvina.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed == true) {
+        throw DispenserJamException()
+    }
     return availableGram - requestedGram
 }
